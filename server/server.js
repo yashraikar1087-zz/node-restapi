942c5e06f8fd638dccadbf1cdb27fe15f4f4c915
Yash Raikar <yashraikar1087@gmail.com>
2018-02-01 10:08:53 -0700

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const{ObjectID}=require('mongodb');
 var express=require('express');
 var bodyParser=require('body-parser');
 
@@ -30,6 +31,23 @@ app.get('/todos',(req,res)=>{
     },(error)=>{
         res.status(400).send(e);
     })
+});
+
+//GET /todos/123342
+app.get('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    //validate id
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+         }
+         res.send({todo});
+    }).catch((e)=>{res.status(400).send()});
+    //res.send(req.params);
 });
 
 app.listen(3000,()=>{

c6d4fff551035bfa038f783c7cc6e758d0e55051
Yash Raikar <yashraikar1087@gmail.com>
2018-02-03 13:01:23 -0700

    setup separate test and dev envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const _=require('lodash');
 
 const{ObjectID}=require('mongodb');
@@ -10,7 +11,7 @@ var {User}=require('./models/user');
 //save new something
 
 var app=express();
-const port=process.env.PORT||3000;
+const port=process.env.PORT;
 
 app.use(bodyParser.json());
 

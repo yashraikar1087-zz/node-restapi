d8e5271dce6b24961c45374e83abacc3bba6426e
Yash Raikar <yashraikar1087@gmail.com>
2018-02-02 17:38:06 -0700

    add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos=[{
     text:"first test todo"
 },{
     _id:new ObjectID(),      
-    text:"second test todo"
+    text:"second test todo",
+    completed:true,
+    completedAt:333
 }];
 
 beforeEach((done)=>{
@@ -133,6 +135,52 @@ describe('DELETE /todos/:id',()=>{
         request(app)
         .delete(`/todos/123`)
         .expect(404)
+        .end(done);
+    });
+});
+
+describe('PATCH /todos/:id',()=>{
+    it('should update the todo',(done)=>{
+        //grab id of first item
+        var hexId=todos[0]._id.toHexString();
+        var text="this should be the new text";
+        request(app).
+        patch(`/todos/${hexId}`).
+        send({
+            completed:true,
+            text
+        })
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            expect(res.body.todo.completedAt).toBeA('number');
+        })
+        .end(done);
+        //update text,set completed true
+        //200
+        //text is changed,completed is true,completedeAt is a number
+    });
+
+    it('should clear completedAt when the todo is not completed',(done)=>{
+        //grab id of second todo item
+        //update text,set completed to false
+        //200
+        //text is changed,completed false, completedAt is null .toNotExist
+        var hexId=todos[1]._id.toHexString();
+        var text="this should be the new text!!2";
+        request(app).
+        patch(`/todos/${hexId}`).
+        send({
+            completed:false,
+            text
+        })
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toNotExist();
+        })
         .end(done);
     });
 });

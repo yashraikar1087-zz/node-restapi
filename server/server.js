cdd66c0e35c81322a32f6fd9b59e1aae8d5c67dc
Yash Raikar <yashraikar1087@gmail.com>
2018-02-11 15:18:28 -0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -105,7 +105,7 @@ app.post('/users',(req,res)=>{
         //res.send(user);
        return user.generateAuthToken();
     }).then((token)=>{
-        res.header('x-auth',token).send(user)
+        res.header('x-auth',token).send(user);
     })
     .catch((e)=>{
         res.status(400).send(e);
@@ -116,6 +116,19 @@ app.post('/users',(req,res)=>{
 
 app.get('/users/me',authenticate,(req,res)=>{
     res.send(req.user);
+});
+
+//POST /users/login {email,password}
+app.post('/users/login',(req,res)=>{
+    var body=_.pick(req.body,['email','password']);
+    //res.send(body);
+    User.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 });
 
 app.listen(port,()=>{
